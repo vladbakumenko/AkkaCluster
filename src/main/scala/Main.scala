306f47800c378289e064463454d30e89be729a9c
vlad bakumenko <klaid-slaid@yandex.ru>
2024-03-11 01:54:39 +0400

    m

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,10 +1,28 @@
-import akka.actor.typed.ActorSystem
 import akka.actor.typed.javadsl.Behaviors
+import akka.actor.typed.{ActorRef, ActorSystem, Behavior}
 import akka.cluster.typed.Cluster
 
 object Main extends App {
+  implicit val system: ActorSystem[MessageListener.Protocol] = ActorSystem(MessageListener.behavior, "messageListener")
 
-  val system = ActorSystem.create(Behaviors.empty, "system")
+  system ! MessageListener.GroupMessage("123")
 
-  val cluster = Cluster(system)
+  system.terminate()
+}
+
+object MessageListener {
+  sealed trait Protocol
+
+  case class GroupMessage(msg: String) extends Protocol
+  case class PrivateMessage(msg: String, replyTo: ActorRef[String]) extends Protocol
+
+  def behavior: Behavior[Protocol] = Behaviors.setup { ctx =>
+    Behaviors.receiveMessage {
+      case GroupMessage(msg) => println(s"group msg: $msg")
+        Behaviors.same
+      case PrivateMessage(msg, replyTo) => println(s"prvt msg: $msg")
+        replyTo ! msg
+        Behaviors.same
+    }
+  }
 }
