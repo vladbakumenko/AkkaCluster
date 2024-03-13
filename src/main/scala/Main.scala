1ae030039534574a7d66f9671d77a4ab2a782bd5
vlad bakumenko <klaid-slaid@yandex.ru>
2024-03-14 01:07:13 +0400

    m

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,13 +1,40 @@
+import akka.actor.Address
 import akka.actor.typed.javadsl.Behaviors
-import akka.actor.typed.{ActorRef, ActorSystem, Behavior}
-import akka.cluster.typed.Cluster
+import akka.actor.typed.{ActorRef, ActorSystem, Behavior, Props}
+import akka.cluster.ClusterEvent
+import akka.cluster.ClusterEvent.{MemberLeft, MemberUp}
+import akka.cluster.typed.{Cluster, Join}
 
 object Main extends App {
-  implicit val system: ActorSystem[MessageListener.Protocol] = ActorSystem(MessageListener.behavior, "messageListener")
 
-  system ! MessageListener.GroupMessage("123")
+  implicit val system: ActorSystem[MemberUp] = ActorSystem(ClusterListener.behavior, "messageListener")
+
+  val cluster = Cluster.apply(system)
+
+  val address = Address("tcp", "messageListener", "127.0.0.1", 2552)
+
+
+  cluster.manager ! Join(address)
+
+  val actor1 = system.systemActorOf(MessageListener.behavior, "actor1", Props.empty)
+
+  actor1 ! MessageListener.GroupMessage("to actor1")
+
+  println(system.address)
+
+  println(cluster.selfMember)
 
   system.terminate()
+}
+
+object ClusterListener {
+
+  def behavior: Behavior[MemberUp] = Behaviors.setup { ctx =>
+    Behaviors.receiveMessage {
+      case MemberUp(member) => println("hello from" + member.address)
+        Behaviors.same
+    }
+  }
 }
 
 object MessageListener {
